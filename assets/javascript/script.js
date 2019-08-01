8a1ce531c11d7e8980a98e49d17d6a6e8863eaed
ernstsamaniego <ernstsamaniego@yahoo.com>
2019-08-01 02:58:17 -0400

    added initial files

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,12 +1,15 @@
+$(document).ready(() => {
 
-
-// https://newsapi.org/docs/get-started
-var url = 'https://newsapi.org/v2/top-headlines?' +
+  // https://newsapi.org/docs/get-started
+  var url = 'https://newsapi.org/v2/top-headlines?' +
     'country=us&' +
     'apiKey=7ccecb2a8e6547c3b9e5242259eeda0e';
-var req = new Request(url);
-fetch(req)
-    .then(function (response) {
-        console.log(response.json());
+  var req = new Request(url);
+  fetch(req)
+    .then(function(response) {
+      console.log(response.json());
     })
 
+
+
+});
